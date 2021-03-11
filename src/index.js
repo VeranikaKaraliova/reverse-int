73181e126031ef89b1ce5abeb282e967388d87d1
VeranikaKaraliova <nika-11@mail.ru>
2021-03-11 22:47:20 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  if(n<0){
+    return +(new String(n).split('').slice(1).reverse().join(''));
+  };
+  return +(new String(n).split('').reverse().join(''));
 }
